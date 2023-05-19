50c7e5a2551b7d4ad609b4d56c3e79c3db9fc382
Shreyas Ingale <127759325+Shreyas-Ingale@users.noreply.github.com>
2023-05-19 18:51:30 +0530

    Fourth Commit: Set up View Engine

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const port = 8000; // default port for express server
 // using Express Router to handle all requests
 app.use('/', require('./routes'));
 
+// set up view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
 // Start server and check if it's running or not
 app.listen(port, function(err){
     if(err){
