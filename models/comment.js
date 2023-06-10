c8d3c6faf08258efff180fa683c35d59a2e9495c
Shreyas Ingale <127759325+Shreyas-Ingale@users.noreply.github.com>
2023-06-10 18:08:01 +0530

    Create Comments Schema

--- models/comment.js
+++ models/comment.js
@@ -1,22 +1,22 @@
 const mongoose = require("mongoose");
 // create schema for user posts
-const postScehma = new mongoose.Schema({
+const commentScehma = new mongoose.Schema({
     content: {
         type: String,
         required: true,
-    },
+    },//User to which the comment belongs to
     user: {
         type: mongoose.Schema.Types.ObjectId, //the unique id for each user console logged one ...
         ref: "User"
-    },//include array of ids of all comments on this post in the schema itself
-    comments: {
+    },//Post to which the comment belongs to
+    post: {
         type: mongoose.Schema.Types.ObjectId, //the unique id for each user console logged one ...
-        ref: "Comment"
+        ref: "Post"
     }
 },{ // gives times at which account(upper fields for a specific user) was created and then whenever it updates
     timestamp: true
 });
 // setting up this as a model/collection with name Post
-const Post = mongoose.model('Post', postScehma);
+const Comment = mongoose.model('Comment', commentScehma);
 
-module.exports = Post;
+module.exports = Comment;
