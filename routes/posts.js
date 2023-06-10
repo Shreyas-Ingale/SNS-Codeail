4c77658437b0026db46029a68bf8f7da25f43c6b
Shreyas Ingale <127759325+Shreyas-Ingale@users.noreply.github.com>
2023-06-10 16:50:00 +0530

    Create User Check

--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const passport = require("passport");
 // import a controller for handling posts 
 const postsController = require('../controllers/posts_controller');
 
-// handle post request on form for posts through /create
-router.post('/create', postsController.create);
+// handle post request on form for posts through /create and also authenticate
+router.post('/create', passport.checkAuthentication, postsController.create);
 
 module.exports = router;
