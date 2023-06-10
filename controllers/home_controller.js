1d476b55482815276dfbe2d09747837f2d3a0941
Shreyas Ingale <127759325+Shreyas-Ingale@users.noreply.github.com>
2023-06-10 16:42:25 +0530

    Create Feed

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,8 +1,28 @@
+const Post = require('../models/post');
+
 //export a home controller for respose to a request in Router
 module.exports.home = function(req, res){
+    // res.cookie('crocodile_id', 20);
     console.log(req.cookies);
-    res.cookie('crocodile_id', 20);
-    return res.render('home', {
-        title: "Home"
+
+    // display all the posts by a user
+    // Post.find({}).then(function(posts){
+    //     return res.render('home', {
+    //         title: "Home",
+    //         posts: posts
+    //     });
+    // }).catch(function(error){
+    //     console.log("Error while finding Posts", error);
+    // });
+
+    // populate the user of each post
+    Post.find({}).populate('user').exec().then(function(posts){
+        return res.render('home', {
+            title: "Home",
+            posts: posts
+        });
+    }).catch(function(error){
+        console.log("Error while finding Posts", error);
     });
+
 }
